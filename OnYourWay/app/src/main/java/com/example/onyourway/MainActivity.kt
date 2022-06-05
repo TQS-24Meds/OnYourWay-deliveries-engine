365ac2cdef8b63c46d7c4b57333a2bf09252b04a
PauloGSP <paulogspereira@ua.pt>
2022-06-05 21:17:55 +0100

    files

--- OnYourWay/app/src/main/java/com/example/onyourway/MainActivity.kt
+++ OnYourWay/app/src/main/java/com/example/onyourway/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.onyourway
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.databinding.DataBindingUtil
+import com.example.onyourway.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
