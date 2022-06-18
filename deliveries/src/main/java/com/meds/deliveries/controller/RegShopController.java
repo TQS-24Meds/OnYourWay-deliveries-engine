46c8493e0703e0443fdfec8a3429854628f1c9a0
PauloGSP <paulogspereira@ua.pt>
2022-06-18 16:53:49 +0100

    controllers

---
+++ deliveries/src/main/java/com/meds/deliveries/controller/RegShopController.java
@@ -0,0 +1,48 @@
+package com.meds.deliveries.controller;
+
+import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.meds.deliveries.exception.ResourceNotFoundException;
+import com.meds.deliveries.model.Admin;
+import com.meds.deliveries.service.AdminService;
+@Controller
+public class RegShopController {
+
+  
+        @Autowired ObjectFactory<HttpSession> httpSessionFactory;
+        @Autowired
+        AdminService adminsv;
+    
+    
+        @GetMapping("/n tem url")
+        public ModelAndView riders(Model model) throws NumberFormatException, ResourceNotFoundException {
+      
+          ModelAndView modelAndView = new ModelAndView();
+      
+          HttpSession session = httpSessionFactory.getObject();
+          String adminid = (String.valueOf(session.getAttribute("admin_id")));
+          Admin admin = adminsv.getAdminById(Integer.parseInt(adminid)).orElseThrow(() -> new ResourceNotFoundException("Admin not found for this id:" + adminid));
+      
+          model.addAttribute("name", admin.getName());
+          
+            //add store list?
+      
+          modelAndView.setViewName("registershop");
+      
+          return modelAndView;
+        }
+      
+    }
+    
+    
